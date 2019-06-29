5f6bd521743f1a6c4ecd963de4dc39cc6e7c617d
jsrimr <jsrimr@naver.com>
2019-06-29 17:34:17 +0900

    일단 돌아가긴 하는 상태의 코드, 단 성능이 별로임

--- Pong/ppo_pong.py
+++ Pong/ppo_pong.py
@@ -14,8 +14,7 @@ import matplotlib.pyplot as plt
 
 import logging
 log_file_name = "ppo_pong.log"
-logging.basicConfig(filename=log_file_name, filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
+logging.basicConfig(filename=log_file_name, filemode='w', format='%(name)s - %(levelname)s - %(message)s', level = logging.DEBUG)
 
 use_cuda = torch.cuda.is_available()
 device   = torch.device("cuda" if use_cuda else "cpu")
@@ -46,67 +45,57 @@ envs = SubprocVecEnv(envs)
 
 env = gym.make(env_name)
 
-def init_weights(m):
-    if isinstance(m, nn.Linear):
-        nn.init.orthogonal_(m.weight)
-        nn.init.constant_(m.bias, 0.1)
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
-
-class CNNBase(nn.Module):
-    def __init__(self, obs_shape, num_outputs,  hidden_size=512):
-        super().__init__()
-
-        # init_ = lambda m: init(m,
-        #     nn.init.orthogonal_,
-        #     lambda x: nn.init.constant_(x, 0),
-        #     nn.init.calculate_gain('relu'))
-
-        self.main = nn.Sequential(
-            nn.Conv2d(1, 32, 8, stride=4),
-            nn.LeakyReLU(),
-            nn.Conv2d(32, 64, 4, stride=2),
-            nn.LeakyReLU(),
-            nn.Conv2d(64, 32, 3, stride=1),
-            nn.LeakyReLU(),
-            Flatten(),
-            nn.Linear(32*6*6, hidden_size),
-            nn.LeakyReLU()
-        )
-
-        # init_ = lambda m: init(m,
-        #     nn.init.orthogonal_,
-        #     lambda x: nn.init.constant_(x, 0))
-
-        self.critic_linear = nn.Linear(hidden_size, num_outputs)
-
-        self.train() # eval 의 반대모드
-
-    def forward(self, inputs, rnn_hxs, ):
-        x = self.main(inputs / 255.0)
-    
-        return self.critic_linear(x), x#return q_value, feature
-
 class ActorCritic(nn.Module):
-    def __init__(self, obs_shape, num_outputs, hidden_size, std=0.0):
-        super(ActorCritic, self).__init__()
-        
-        self.base = CNNBase(mini_batch_size, num_outputs)
-        self.actor = nn.Sequential(
-            nn.Linear(512, 32),
-            nn.LeakyReLU(),
-            nn.Linear(32, num_outputs),
-            nn.Softmax(dim = -1)
-        )
-        
-    def forward(self, x):
-        q_value, actor_features = self.base(x, num_outputs)
-        
-        policy    = self.actor(actor_features)
-        
-        return policy, q_value
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1,
+                               out_channels=32,
+                               kernel_size=8,
+                               stride=4,
+                               padding=0)
+        nn.init.orthogonal_(self.conv1.weight, np.sqrt(2))
+#The second convolution layer takes a 20x20 frame and produces a 9x9 frame
+        self.conv2 = nn.Conv2d(in_channels=32,
+                               out_channels=64,
+                               kernel_size=4,
+                               stride=2,
+                               padding=0)
+        nn.init.orthogonal_(self.conv2.weight, np.sqrt(2))
+#The third convolution layer takes a 9x9 frame and produces a 7x7 frame
+        self.conv3 = nn.Conv2d(in_channels=64,
+                               out_channels=64,
+                               kernel_size=3,
+                               stride=1,
+                               padding=0)
+        nn.init.orthogonal_(self.conv3.weight, np.sqrt(2))
+#A fully connected layer takes the flattened frame from thrid convolution layer, and outputs 512 features
+
+        self.lin = nn.Linear(in_features=6 * 6 * 64,
+                             out_features=512)
+        nn.init.orthogonal_(self.lin.weight, np.sqrt(2))
+#A fully connected layer to get logits for ππ
+        self.pi_logits = nn.Linear(in_features=512,
+                                   out_features=6)
+        nn.init.orthogonal_(self.pi_logits.weight, np.sqrt(0.01))  # softmax 없어도 괜찮을까? -> relu 이기 때문에 괜찮다. 음수 안들어간다
+#A fully connected layer to get value function
+        self.value = nn.Linear(in_features=512,
+                               out_features=1)
+        nn.init.orthogonal_(self.value.weight, 1)
+    
+    def forward(self, obs):
+
+        h = F.relu(self.conv1(obs))
+        h = F.relu(self.conv2(h))
+        h = F.relu(self.conv3(h))
+        h = h.reshape((-1, 6 * 6 * 64))
+
+        h = F.relu(self.lin(h))
+
+        pi = Categorical(logits=self.pi_logits(h))
+        value = self.value(h)
+
+        return pi, value
+
 
 def plot(frame_idx, rewards):
     # clear_output(True)
@@ -124,8 +113,8 @@ def test_env(vis=False):
     while not done:
         state = preprocess_single(state)
         state = state.expand(1,1,80,80)
-        prob, _ = model(state)
-        dist = Categorical(prob)
+        dist, _ = model(state)
+        # dist = Categorical(prob)
         next_state, reward, done, _ = env.step(dist.sample().cpu().numpy()[0])
         state = next_state
         if vis: env.render()
@@ -134,6 +123,7 @@ def test_env(vis=False):
     return total_reward
 
 def compute_gae(next_value, rewards, masks, values, gamma=0.99, tau=0.95):
+    
     values = values + [next_value]
     gae = 0
     returns = []
@@ -145,36 +135,37 @@ def compute_gae(next_value, rewards, masks, values, gamma=0.99, tau=0.95):
 
 def ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantage): # 전체 배치에서 mini_batch 를 만드는 것이다.
     batch_size = states.size(0)
-    for _ in range(batch_size // mini_batch_size):
-        rand_ids = np.random.randint(0, batch_size, mini_batch_size)
-        
-        yield states[rand_ids], actions[rand_ids], log_probs[rand_ids], returns[rand_ids], advantage[rand_ids]
-        
+    ids = np.random.permutation(batch_size)
+    ids = np.split(ids, batch_size // mini_batch_size)
+    for i in range(len(ids)):
+        yield states[ids[i], :], actions[ids[i], :], log_probs[ids[i], :], returns[ids[i], :], advantage[ids[i], :]
         
 
 def ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantages, clip_param=0.2): # training
     for _ in range(ppo_epochs):
         for state, action, old_log_probs, return_, advantage in ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantages):
-            pi, value = model(state)
+            dist, vlaue = model(state)
+            new_log_probs = dist.log_prob(action)
             
-            
-            pi_a = pi.gather(1,action.unsqueeze(-1))
-            logging.warning(f'{pi_a} : pi_a')
-            new_log_probs = torch.log(pi_a)
+            #pi, value = model(state)
+            # pi_a = pi.gather(1,action.unsqueeze(-1))
+            # logging.warning(f'{pi_a} : pi_a')
+            # new_log_probs = torch.log(pi_a)
 
             ratio = (new_log_probs - old_log_probs).exp()
             # print("ratio :",  ratio)
             surr1 = ratio * advantage
             surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
 
+
             with torch.autograd.detect_anomaly():
                 actor_loss  = - torch.min(surr1, surr2).mean()
                 # print("actor loss", actor_loss)
                 critic_loss = (return_.detach() - value).pow(2).mean()
                 # print("critic loss", critic_loss)
-                # entropy = Categorical(pi).entropy()
+                entropy = dist.entropy()
 
-                loss = 0.5 * critic_loss + actor_loss # - 0.001 * entropy
+                loss = 0.5 * critic_loss + actor_loss  - 0.01 * entropy
 
                 optimizer.zero_grad()
                 
@@ -190,8 +181,8 @@ num_outputs = envs.action_space.n
 
 state = envs.reset()
 
-model = ActorCritic(state.shape, num_outputs, hidden_size).to(device) #return dist, v
-model.load_state_dict(torch.load(f'weight/pong_{load_weight_n}.pt'))
+model = ActorCritic().to(device) #return dist, v
+# model.load_state_dict(torch.load(f'weight/pong_{load_weight_n}.pt'))
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
 max_frames = 150000
@@ -215,16 +206,17 @@ while not early_stop:
         state = preprocess_batch(state)
         
         dist, value = model(state)
+        # print(value, value.shape , "value")
+        # m = Categorical(dist)
         
-        m = Categorical(dist)
-        
-        action = m.sample()
+        action = dist.sample()
         
         
         next_state, reward, done, _ = envs.step(action.cpu().numpy()) #done 한다고 끝내질 않네??
         
         # logging.warning(f'dist[action] : {dist[action]}')
-        log_prob = torch.log(dist[action])
+        # print(action)
+        log_prob = dist.log_prob(action) #torch.log(dist[action])
         log_probs.append(log_prob)
         values.append(value)
         rewards.append(torch.FloatTensor(reward).unsqueeze(1).to(device))
@@ -258,14 +250,20 @@ while not early_stop:
     
     #num_step 만큼 진행했을 때 reward 얼마였는지 구하기
     next_state = preprocess_batch(next_state)
+#     print("next_state shape: ", next_state.shape) # [16, 1, 80,80]
     _, next_value = model(next_state)
+#     print("next_vlaue shape: " , next_value.shape)
     returns = compute_gae(next_value, rewards, masks, values)
-
-    returns   = torch.cat(returns).detach()
+#     logging.debug(f"{returns} and shape is {len(returns)}, {len(returns[0])}" )
+    returns = torch.cat(returns).detach()
+#     logging.debug("after")
+#     logging.debug(f"{returns} and shape is {returns.shape}" )
+#     print(returns.shape, "what's happening here?")
     log_probs = torch.cat(log_probs).detach()
     values    = torch.cat(values).detach()
     states    = torch.cat(states)
     actions   = torch.cat(actions)
+    
     advantage = returns - values
     
-    ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantage)
+ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantage)
